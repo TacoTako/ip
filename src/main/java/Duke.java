af7a41d3c7a27884722f65222bf225916733e602
Tan I-hur <ihur.tan@gmail.com>
2026-01-19 17:22:24 +0800

    Add user greeting
    Let's write a greeting and goodbye function
    
    The main function can directly call these functions, where the internal workings can be
    substituted later in the project.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLine();
+        greet();
+        printLine();
+        sayGoodbye();
+        printLine();
+    }
+
+    private static void greet() {
+        String greeting =  "Hello! I'm Conniving Eunuch, the Duplicitous\nWhat can I do for you?";
+        System.out.println(greeting);
+    }
+
+    private static void sayGoodbye() {
+        String bye =  "Bye. Hope to see you again soon!";
+        System.out.println(bye);
+    }
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
