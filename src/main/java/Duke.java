d04c2460a34fcc8920787740b3a001f2c0d4c788
Tan I-hur <ihur.tan@gmail.com>
2026-01-19 17:38:02 +0800

    Add listen and echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        printLine();
+        Scanner s = new Scanner(System.in);
         greet();
-        printLine();
+
+        //while loop for listening to users
+        while (true) {
+            String input = s.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(formatOutput(input));
+            }
+        }
+
         sayGoodbye();
-        printLine();
     }
 
     private static void greet() {
         String greeting =  "Hello! I'm Conniving Eunuch, the Duplicitous\nWhat can I do for you?";
-        System.out.println(greeting);
+        System.out.println(formatOutput(greeting));
     }
 
     private static void sayGoodbye() {
         String bye =  "Bye. Hope to see you again soon!";
-        System.out.println(bye);
+        System.out.println(formatOutput(bye));
     }
 
-    private static void printLine() {
-        System.out.println("____________________________________________________________");
+    private static String formatOutput(String s) {
+        String line = "____________________________________________________________";
+        return line + "\n" + s + "\n" +line;
     }
 }
