b611994f5759cff853204855e1aea3f9525418b9
Tan I-hur <ihur.tan@gmail.com>
2026-01-20 16:52:13 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private boolean isDone = false;
+    private final String taskDesc;
+
+    public Task(String desc) {
+        this.taskDesc = desc;
+    }
+
+    public boolean getDone() {
+        return isDone;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void unmarkDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String check = (isDone) ? " " : "X";
+        return "[" + check + "] " + taskDesc;
+    }
+}
