35f52e069dd8e55f41953a636450656b5199e2db
Tan I-hur <ihur.tan@gmail.com>
2026-01-29 23:40:14 +0800

    Add more tests to TaskListTest

--- src/test/java/eunuch/task/TaskListTest.java
+++ src/test/java/eunuch/task/TaskListTest.java
@@ -6,6 +6,25 @@ import org.junit.jupiter.api.Test;
 
 public class TaskListTest {
     @Test
+    public void createTaskListTest() {
+        TaskList list = new TaskList();
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void emptyTaskListStringTest() {
+        TaskList list = new TaskList();
+        assertEquals("", list.toString());
+    }
+
+    @Test
+    public void taskListStringTest() {
+        TaskList list = new TaskList();
+        list.add(new TaskStub("test"));
+        assertEquals("1. [ ] test", list.toString());
+    }
+
+    @Test
     public void addOneTaskTest() {
         TaskList list = new TaskList();
         list.add(new TaskStub("test"));
